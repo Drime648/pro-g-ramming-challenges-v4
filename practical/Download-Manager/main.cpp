546050f0b275293865b2cf43dcbb53c491aadb31
Your name here <your_email@example.com>
2023-07-14 10:06:32 -0700

    initial files

--- practical/Download-Manager/main.cpp
+++ practical/Download-Manager/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
-
+#include <string>
+#include <stdio.h>
+#include <curl/curl.h>
+using std::string;
+using std::cin;
+using std::cout;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "this is a download manager\n" <<
+    "please input a URL to download\n";
+
+    string url;
+    cin >> url;
+    cout << "your url is: " << url;
+
+    //step 1: make sure URL is valid
+    //Make a simple HTTP GET request and make sure the response code is 200
+
+
+
+
     return 0;
 }
